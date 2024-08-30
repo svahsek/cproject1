70f33cae9f257f26eb8b47431057b6d415b6bfa1
Keshaw Singh <117708272+keshawscom@users.noreply.github.com>
2024-08-30 15:13:24 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+// https://www.youtube.com/watch?v=8jo3Dy5CA2Y&t=718s
+#include <stdio.h>
+
+int main() {
+// declare variables to store two numbers
+int num1 = 5;
+int num2 = 10;
+
+// calculate the sum of two numbers
+int sum = num1 + num2;
+
+// Display the result 
+printf("the sum of %d and %d is: %d\n", num1, num2, sum);
+
+//return 0 to indicate successful execution of the program
+return 0;
+
+}
