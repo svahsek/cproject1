eb2470ff0b080f7d10bdb03fb9587cf75b4f80b9
KESHAVSINGH <ks@keshaws.com>
2024-08-31 08:33:32 +0530

    3rd

--- second.c
+++ second.c
@@ -1,11 +1,10 @@
-// https://www.youtube.com/watch?v=8jo3Dy5CA2Y&t=718s
 #include <stdio.h>
 
 int main() {
 // declare variables to store two numbers
+
 int num1 = 5;
 int num2 = 10;
-int num3 = 15;
 
 // calculate the sum of two numbers
 int sum = num1 + num2;
