42c908cf7d110037e24fc99cae4c72867c2cc883
KESHAVSINGH <ks@keshaws.com>
2024-09-03 20:31:41 +0530

    salary file fix with co pilot

--- salary.c
+++ salary.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-main()
+int main()
 {
 float gross_salary, net_salary;
 
@@ -8,4 +8,6 @@ printf("Enter gross salary of an employee\n"); scanf("%f ",&gross_salary );
 if(gross_salary <10000) net_salary= gross_salary;
 if(gross_salary >= 10000)
 net_salary = gross_salary- 0.15*gross_salary;
-printf("\nNet salary is Rs.%.2f\n", net_salary); }
+printf("\nNet salary is Rs.%.2f\n", net_salary);
+return 0;
+}
