646baffe3e7f67e4fc017c75b17b3dfc5fe4d57c
KESHAVSINGH <ks@keshaws.com>
2024-09-03 20:16:16 +0530

    modified:   salary.c

--- salary.c
+++ salary.c
@@ -2,8 +2,10 @@
 main()
 {
 float gross_salary, net_salary;
-printf(“Enter gross salary of an employee\n”); scanf(“%f ”,&gross_salary );
+
+printf("Enter gross salary of an employee\n"); scanf("%f ",&gross_salary );
+
 if(gross_salary <10000) net_salary= gross_salary;
 if(gross_salary >= 10000)
 net_salary = gross_salary- 0.15*gross_salary;
-printf(“\nNet salary is Rs.%.2f\n”, net_salary); }
+printf("\nNet salary is Rs.%.2f\n", net_salary); }
